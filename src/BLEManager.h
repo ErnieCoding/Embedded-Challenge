44b9ca2fd664b72b12795dd3ebebbfdfd8652299
jx3057-svg <jx3057@nyu.edu>
2025-12-14 11:32:10 -0500

    Update BLEManager.h

--- src/BLEManager.h
+++ src/BLEManager.h
@@ -2,9 +2,9 @@
 
 #include "mbed.h"
 #include "ble/BLE.h"
-#include "ble/gap/Gap.h"
-#include "ble/gap/AdvertisingDataBuilder.h"
-#include "events/EventQueue.h"
+#include "ble/GattCharacteristic.h"
+#include "ble/GattService.h"
+#include "ble/gap/AdvertisingDataSimpleBuilder.h"
 
 class BLEManager {
 public:
@@ -13,26 +13,28 @@ public:
     void init();
     void process();
 
-    void updateTremor(uint8_t level);
-    void updateDyskinesia(uint8_t level);
-    void updateFOG(uint8_t state);
+    void updateTremor(uint8_t score);
+    void updateDyskinesia(uint8_t score);
+    void updateFOG(uint8_t score);
 
 private:
-    /* BLE core */
     BLE &_ble;
-    events::EventQueue _event_queue;
 
-    /* BLE values */
     uint8_t _tremor;
     uint8_t _dyskinesia;
     uint8_t _fog;
 
-    /* BLE callbacks */
-    void onInitComplete(BLE::InitializationCompleteCallbackContext *params);
-    static void scheduleBleEvents(BLE::OnEventsToProcessCallbackContext *context);
+    uint8_t _lastTremor;
+    uint8_t _lastDyskinesia;
+    uint8_t _lastFOG;
 
-    /* BLE characteristics */
     GattCharacteristic *_tremorChar;
     GattCharacteristic *_dyskinesiaChar;
     GattCharacteristic *_fogChar;
+
+    void onInitComplete(BLE::InitializationCompleteCallbackContext *params);
+
+    static void scheduleBleEvents(
+        BLE::OnEventsToProcessCallbackContext *context
+    );
 };
