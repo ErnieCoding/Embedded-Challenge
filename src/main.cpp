8636983230be5686c07885679c91943802bf391e
ErnieCoding <saakianernie@gmail.com>
2025-12-14 02:37:38 -0500

    Removed local references for CMIS-DSP lib

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ int main() {
     Timer blinkTimer;
     blinkTimer.start();
     
-    //float blinkInterval = 0.5f;
+    float blinkInterval = 0.5f;
     uint32_t sampleCount = 0;
     
     while (true) {
@@ -46,27 +46,26 @@ int main() {
         sensors.readAccel(ax, ay, az);
         sensors.readGyro(gx, gy, gz);
         
-        //float intensity = sensors.computeMotionIntensity(ax, ay, az, gx, gy, gz);
+        float intensity = sensors.computeMotionIntensity(ax, ay, az, gx, gy, gz);
         
-        // LED INTENSITY BLINKING - CAN USE FOR TEST
-        // if (intensity < 0.5f) { // threshold for no motion
-        //     led = 0;
-        //     blinkTimer.reset();
-        // } else {
-        //     float clamped = intensity;
-        //     if (clamped > 5.0f) clamped = 5.0f; // max blink rate
-        //     blinkInterval = 0.5f / clamped;
+        if (intensity < 0.5f) { // threshold for no motion
+            led = 0;
+            blinkTimer.reset();
+        } else {
+            float clamped = intensity;
+            if (clamped > 5.0f) clamped = 5.0f; // max blink rate
+            blinkInterval = 0.5f / clamped;
 
-        //     if (blinkTimer.elapsed_time().count() / 1e6f > blinkInterval) {
-        //         led = !led;
-        //         blinkTimer.reset();
-        //     }
-        // }
+            if (blinkTimer.elapsed_time().count() / 1e6f > blinkInterval) {
+                led = !led;
+                blinkTimer.reset();
+            }
+        }
 
-        printf("\n\n\n===============================\n\n");
-        printf(">Raw_Acc: [%.2f]  [%.2f]  [%.2f]\n", ax, ay, az);
-        printf(">Raw_Gyro: [%.2f]  [%.2f]  [%.2f]\n\n", gx, gy, gz);
-        printf("---");
+        // printf("\n\n\n===============================\n\n");
+        // printf(">Raw_Acc: [%.2f]  [%.2f]  [%.2f]\n", ax, ay, az);
+        // printf(">Raw_Gyro: [%.2f]  [%.2f]  [%.2f]\n\n", gx, gy, gz);
+        // printf("---");
 
         float acc_mag  = sqrtf(ax*ax + ay*ay + az*az);
         float gyro_mag = sqrtf(gx*gx + gy*gy + gz*gz);
@@ -80,17 +79,17 @@ int main() {
             // Processing gyroscope data
             gyro_buffer.process();
 
-            printf(">ACC_DOM_Freq_MAG: [%.2f]\n", acc_buffer.dominantMag);
-            printf(">ACC_DOM_Freq_Hz: [%.2f]\n\n", acc_buffer.dominantHz);
+            // printf(">ACC_DOM_Freq_MAG: [%.2f]\n", acc_buffer.dominantMag);
+            // printf(">ACC_DOM_Freq_Hz: [%.2f]\n\n", acc_buffer.dominantHz);
 
-            printf(">GYRO_DOM_Freq_MAG: [%.2f]\n", gyro_buffer.dominantMag);
-            printf(">GYRO_DOM_Freq_Hz: [%.2f]\n\n", gyro_buffer.dominantHz);
+            // printf(">GYRO_DOM_Freq_MAG: [%.2f]\n", gyro_buffer.dominantMag);
+            // printf(">GYRO_DOM_Freq_Hz: [%.2f]\n\n", gyro_buffer.dominantHz);
 
             acc_buffer.reset();
             gyro_buffer.reset();
         }
 
-        printf("===============================\n\n\n");
+        // printf("===============================\n\n\n");
 
 
 
