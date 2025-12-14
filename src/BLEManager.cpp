cc68a3f19e651ee07f1749d3960806aa4a3376d1
jx3057-svg <jx3057@nyu.edu>
2025-12-14 11:32:26 -0500

    Update BLEManager.cpp

--- src/BLEManager.cpp
+++ src/BLEManager.cpp
@@ -1,5 +1,7 @@
 #include "BLEManager.h"
 
+static events::EventQueue bleEventQueue;
+
 static const UUID MOTION_SERVICE_UUID("12345678-1234-5678-1234-56789abc0000");
 static const UUID TREMOR_UUID        ("12345678-1234-5678-1234-56789abc0001");
 static const UUID DYSKINESIA_UUID    ("12345678-1234-5678-1234-56789abc0002");
@@ -7,9 +9,8 @@ static const UUID FOG_UUID           ("12345678-1234-5678-1234-56789abc0003");
 
 BLEManager::BLEManager()
     : _ble(BLE::Instance()),
-      _tremor(0),
-      _dyskinesia(0),
-      _fog(0),
+      _tremor(0), _dyskinesia(0), _fog(0),
+      _lastTremor(0), _lastDyskinesia(0), _lastFOG(0),
       _tremorChar(nullptr),
       _dyskinesiaChar(nullptr),
       _fogChar(nullptr) {}
@@ -17,16 +18,27 @@ BLEManager::BLEManager()
 void BLEManager::init() {
     _ble.onEventsToProcess(scheduleBleEvents);
     _ble.init(this, &BLEManager::onInitComplete);
+
     static Thread bleThread;
-    bleThread.start(callback(&_event_queue, &events::EventQueue::dispatch_forever));
+    bleThread.start(callback(&bleEventQueue, &events::EventQueue::dispatch_forever));
 }
 
-void BLEManager::onInitComplete(BLE::InitializationCompleteCallbackContext *params) {
+void BLEManager::scheduleBleEvents(
+    BLE::OnEventsToProcessCallbackContext *context
+) {
+    bleEventQueue.call(
+        callback(&context->ble, &BLE::processEvents)
+    );
+}
+
+void BLEManager::onInitComplete(
+    BLE::InitializationCompleteCallbackContext *params
+) {
     if (params->error != BLE_ERROR_NONE) {
         printf("BLE init failed\n");
         return;
     }
-    
+
     _tremorChar = new GattCharacteristic(
         TREMOR_UUID, &_tremor, sizeof(_tremor), sizeof(_tremor),
         GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY
@@ -51,21 +63,22 @@ void BLEManager::onInitComplete(BLE::InitializationCompleteCallbackContext *para
     GattService motionService(
         MOTION_SERVICE_UUID,
         chars,
-        sizeof(chars) / sizeof(chars[0])
+        3
     );
 
     _ble.gattServer().addService(motionService);
 
-    uint8_t advBuffer[31];  
+    uint8_t advBuffer[31];
     ble::AdvertisingDataSimpleBuilder<sizeof(advBuffer)> advData;
 
     advData.setFlags();
     advData.setName("Parkinson-Monitor");
+
     _ble.gap().setAdvertisingParameters(
         ble::LEGACY_ADVERTISING_HANDLE,
         ble::AdvertisingParameters(
             ble::advertising_type_t::CONNECTABLE_UNDIRECTED,
-            ble::adv_interval_t(160)   // 100ms
+            ble::adv_interval_t(160)
         )
     );
 
@@ -76,5 +89,5 @@ void BLEManager::onInitComplete(BLE::InitializationCompleteCallbackContext *para
 
     _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
 
-    printf("BLE advertising started");
+    printf("BLE advertising started\n");
 }
